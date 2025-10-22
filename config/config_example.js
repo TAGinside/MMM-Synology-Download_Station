6aaa79aea626ffdb68b43001f456e375e350942f
TAGinside <tag.costa@bluewin.ch>
2025-10-22 18:53:26 +0200

    test

--- config/config_example.js
+++ config/config_example.js
@@ -2,7 +2,7 @@
   module: "MMM-Synology-Download_Station",
   position: "top_center",
   config: {
-    host: "IP/DNS",      // IP/DNS de votre Synology
+    host: "IP/DNS",             // IP/DNS de votre Synology
     port: 5001,                 // Port HTTPS DSM default
     useHttps: true,             // Using HTTPS to connect to DSM
     user: "username",           
